b0b19f5182c1cd290125f8e2099466fd7e0eaee3
Sergio Serrano <serranohsa@hotmail.com>
2021-06-04 21:40:32 -0600

    Add cars.json and method

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req,res,next)=>{
-  res.send("My server ;)")
+app.get('/cars', function(req,res){
+  const data = require('./cars.json')
+  res.json(data);
 })
+app.use(function(req, res, next) {
+  res.status(404).send('No pudimos encontrar lo que buscas');
+  res.status(500).send('Something broke!');
+});
 //heroku injects the port number into the PORT env value
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
-  console.log(`Listening on port:${port}`);
+  //console.log(`Listening on port:${port}`);
+  console.log(`Running`);
 });
   
