cea83509be6100e7bf6868306a84b3002d366ba6
Sergio Serrano <serranohsa@hotmail.com>
2021-06-07 13:08:09 -0600

    index update

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
 const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/cars', function(req,res){
-  const data = require('./cars.json')
+app.get("/cars", function (req, res) {
+  const data = require("./cars.json");
   res.json(data);
-})
-app.use(function(req, res, next) {
-  res.status(404).send('No pudimos encontrar lo que buscas');
-  res.status(500).send('Something broke!');
+});
+app.use(function (req, res, next) {
+  res.status(404).send("No pudimos encontrar lo que buscas");
+  res.status(500).send("Something broke!");
 });
 //heroku injects the port number into the PORT env value
 const port = process.env.PORT || 3001;
@@ -20,4 +19,3 @@ app.listen(port, () => {
   //console.log(`Listening on port:${port}`);
   console.log(`Running`);
 });
-  
