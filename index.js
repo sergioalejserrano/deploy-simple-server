125be0bbc680963a65fe7f43ed880030fc2b6fa2
Sergio Serrano <serranohsa@hotmail.com>
2021-06-17 19:08:31 -0600

    add CORS

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require("express");
-
+const cors = require ('cors');
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(cors());
 
-app.get("/cars", function (req, res) {
+app.get('/cars', function (req, res, next) {
   const data = require("./cars.json");
   res.json(data);
-});
+})
+
+// app.get("/cars", function (req, res) {
+//   const data = require("./cars.json");
+//   res.json(data);
+// });
 app.use(function (req, res, next) {
   res.status(404).send("No pudimos encontrar lo que buscas");
   res.status(500).send("Something broke!");
 });
 //heroku injects the port number into the PORT env value
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001 || 80;
 
 app.listen(port, () => {
   //console.log(`Listening on port:${port}`);
